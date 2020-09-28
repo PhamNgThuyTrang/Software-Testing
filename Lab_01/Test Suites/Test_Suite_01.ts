d227ec0ef857719c0741d104122d13004b66e032
pntt2202 <pntt220200@gmail.com>
2020-09-28 13:56:15 +0700

    Lab_01

---
+++ Lab_01/Test Suites/Test_Suite_01.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test_Suite_01</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>a964e803-8a20-42ce-b8db-f3194adb7e87</testSuiteGuid>
+   <testCaseLink>
+      <guid>dbb6685d-3f81-480e-bb7e-d34a77328d06</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test_case_01</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c3a7db9d-28a9-4f2e-a0c6-f78efe639c21</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test_case_02</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fe2ad527-a12b-45ea-ab6b-f2cdc7a9e0b6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test_case_03</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f18feed7-e0ab-4ff9-a1af-0cd8780c8e10</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test_case_04</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2a8c425f-427a-400f-949e-788bae4d2a2c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test_case_05</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
